2cadcfe71b528c8e6ff68e74678c1316cf3927b3
Sanatjon Burkhanov <sburhanov1977@gmail.com>
2025-05-24 19:45:59 -0400

    bored at work so made a little to do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,106 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { link } from "fs";
 
 function App() {
+  type Task = {
+    text: string;
+    completed: boolean;
+  };
+
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [loaded, setLoaded] = useState(false);
+  const [doneTasks, setDoneTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    const saved = localStorage.getItem("tasks");
+    console.log("Loading local storage " + saved);
+    if (saved) {
+      setTasks(JSON.parse(saved));
+    }
+    setLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    if (loaded) {
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+    console.log("Saved to localStorage:", tasks);
+  }, [tasks, loaded]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "20px" }}>
+      <h2>My To-Do Lists</h2>
+
+      <input
+        type="text"
+        placeholder="Type something..."
+        value={task}
+        onChange={(e) => {
+          setTask(e.target.value);
+        }}
+      />
+      <button
+        onClick={(e) => {
+          if (task.trim() === "") return;
+          const newTasks = { text: task, completed: false };
+          setTasks((prev) => [...prev, newTasks]);
+
+          setTimeout(() => {
+            setTasks((prev) => prev.filter((task) => task !== newTasks));
+            setDoneTasks((prev) => [...prev, { ...newTasks, completed: true }]);
+          }, 5000);
+        }}
+      >
+        Add
+      </button>
+
+      {/* The list here  */}
+      <h3>Active Tasks</h3>
+      <ul>
+        {tasks.map((task_, i) => (
+          <li
+            key={i}
+            style={{
+              textDecoration: task_.completed ? "line-through" : "none",
+              marginBottom: "10px",
+            }}
+          >
+            {task_.text}
+            <button
+              onClick={() => {
+                const updated = tasks.filter((_, index) => index !== i);
+                setTasks(updated);
+              }}
+              style={{ marginLeft: "10px" }}
+            >
+              ✔️
+            </button>
+            <button
+              onClick={() => {
+                const updated = [...tasks];
+                updated.splice(i, 1);
+                setTasks(updated);
+              }}
+              style={{ marginLeft: "5px" }}
+            >
+              ❌
+            </button>
+          </li>
+        ))}
+      </ul>
+      <h3 style={{ marginTop: "30px" }}>✅ Done</h3>
+      <ul>
+        {doneTasks.map((task, index) => (
+          <li
+            key={index}
+            style={{ color: "gray", textDecoration: "line-through" }}
+          >
+            {task.text}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
